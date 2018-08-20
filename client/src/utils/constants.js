e95a451f64db6f7d9a0ea4bce2334a1cd694342a
Alex Lee <alex@uniteus.com>
2018-08-20 02:46:23 -0400

    Manually add deployed contract of rinkeby survey

---
+++ client/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const rinkebySurveyFactoryAddress = '0x7e57b583ee65a423455aedfcd870d55b2a1150e9'; /* eslint-disable-line */
--- client/src/utils/getContractInstance.js
+++ client/src/utils/getContractInstance.js
@@ -1,7 +1,21 @@
+import { rinkebySurveyFactoryAddress } from './constants';
+
 const getContractInstance = async (web3, contractDefinition) => {
+  let deployedAddress;
+  const networkType = await web3.eth.net.getNetworkType();
   // get network ID and the deployed address
   const networkId = await web3.eth.net.getId();
-  const deployedAddress = contractDefinition.networks[networkId].address;
+
+  switch (networkType) {
+    case 'rinkeby': {
+      console.log('hitting here');
+      deployedAddress = rinkebySurveyFactoryAddress;
+      break;
+    }
+    default: {
+      deployedAddress = contractDefinition.networks[networkId].address;
+    }
+  }
 
   // create the instance
   const instance = new web3.eth.Contract(contractDefinition.abi, deployedAddress);
